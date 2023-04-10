f3e4d68d9a231655f973ecde65b2e30986795fde
Chris Wilson <chrisw@myself.com>
2023-04-09 23:45:05 -0400

    initial commit and basic structure, plus a few bonus completes

---
+++ server.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const server = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'Very secret',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+  server.use(session(sess));
+
+  server.engine('handlebars', hbs.engine);
+server.set('view engine', 'handlebars');
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  server.listen(PORT, () => console.log('Now listening'));
+});
